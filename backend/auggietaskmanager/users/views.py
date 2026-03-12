f30a72b9dc03f25b1d542fdca45d95b7e530aa9d
Luis Alvarado <alvaradl@augsburg.edu>
2026-03-12 11:48:56 -0500

    - Added login URL and currently updating views for authentication - Added a new login URL to the users

--- backend/auggietaskmanager/users/views.py
+++ backend/auggietaskmanager/users/views.py
@@ -1,9 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
 
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.contrib.auth import authenticate
 
+from django.shortcuts import render
 from .models import UserProfile
 
 # Create your views here.
