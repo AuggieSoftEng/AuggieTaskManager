a2b36278f09d87beaba8e294a8cb567e51c240bf
Dustin Davidson <davidsod@augsburg.edu>
2026-03-11 20:14:53 -0500

    FIXED THE MOST ANNOYING ERROR IVE EVER SEEN

--- frontend/AuggieTaskManagerFrontend/forge.config.ts
+++ frontend/AuggieTaskManagerFrontend/forge.config.ts
@@ -10,6 +10,7 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
+import { preloadConfig } from './webpack.preload.config';
 
 const config: ForgeConfig = {
   packagerConfig: {
@@ -36,6 +37,7 @@ const config: ForgeConfig = {
             js: './src/renderer/renderer.tsx',
             name: 'main_window',
             preload: {
+              config: preloadConfig, // 👈 add this
               js: './src/preload/preload.ts',
             },
           },
