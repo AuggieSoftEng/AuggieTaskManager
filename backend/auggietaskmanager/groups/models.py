c0c1d61a1a793f367cc2d932f4b33f3895bf5091
Dustin Davidson <davidsod@augsburg.edu>
2026-03-25 16:56:23 -0500

    Created the model for StudyGroup, and migrated

--- backend/auggietaskmanager/groups/models.py
+++ backend/auggietaskmanager/groups/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class StudyGroup(models.Model):
+    name = models.CharField(max_length=100)
+
+    description = models.TextField(max_length=500, blank=True)
+
+    members = models.ManyToManyField(User, related_name='study_groups')
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_groups')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
