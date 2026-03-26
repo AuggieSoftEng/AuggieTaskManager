d68d732ceb55c079f202b5e1e61811510d80429d
Dustin Davidson <davidsod@augsburg.edu>
2026-03-26 11:38:24 -0500

    Updated admin.py, fixed urls.py

--- backend/auggietaskmanager/groups/urls.py
+++ backend/auggietaskmanager/groups/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "groups"
 
 urlpatterns = [
-    path("groups/", views.StudyGroupListCreateView.as_view(), name="groups"),
-    path("joinGroup/", views.join_study_group, name="joinGroup"),
-    path("leaveGroup/", views.leave_study_group, name="leaveGroup"),
+    path("", views.StudyGroupListCreateView.as_view(), name="groups"),
+    path("join/", views.join_study_group, name="joinGroup"),
+    path("leave/", views.leave_study_group, name="leaveGroup"),
 ]
